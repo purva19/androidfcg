63d24fd12ede0a5826c67d135bb3c072001ace16
srngirish@gmail.com <srngirish@gmail.com@6f429456-4490-fa68-301e-5b390702f549>
2012-06-20 15:08:07 +0000

    Updated the first page with image and the button

--- src/com/faultcodeguide/FaultCodeGuideActivity.java
+++ src/com/faultcodeguide/FaultCodeGuideActivity.java
@@ -2,12 +2,22 @@ package com.faultcodeguide;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class FaultCodeGuideActivity extends Activity {
+public class FaultCodeGuideActivity extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Button button = (Button) findViewById(R.id.button1);
+		button.setOnClickListener(this);
     }
+
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 }
