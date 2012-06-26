79422ec12b739fe76992e5a39eb6e31ef4e87497
srngirish@gmail.com <srngirish@gmail.com@6f429456-4490-fa68-301e-5b390702f549>
2012-06-26 12:10:10 +0000

    Select Series Working

--- trunk/gen/com/faultcodeguide/R.java
+++ trunk/gen/com/faultcodeguide/R.java
@@ -15,11 +15,13 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int button1=0x7f050001;
-        public static final int uk=0x7f050000;
+        public static final int button1=0x7f050002;
+        public static final int name=0x7f050000;
+        public static final int uk=0x7f050001;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int list_view=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int Button_Title=0x7f040002;
--- trunk/src/com/faultcodeguide/FaultCodeGuideActivity.java
+++ trunk/src/com/faultcodeguide/FaultCodeGuideActivity.java
@@ -1,10 +1,15 @@
 package com.faultcodeguide;
 
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class FaultCodeGuideActivity extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
@@ -12,12 +17,36 @@ public class FaultCodeGuideActivity extends Activity implements OnClickListener 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+       
+        DataBaseHelper myDbHelper = new DataBaseHelper(this);
+        ///WE WILL cpo
+        try {
+        	//System.out.println("I M CLICKED");
+        	myDbHelper.createDataBase();
+        }//end of try 
+        catch (IOException ioe) {
+        	throw new Error("Unable to create database");
+        	}//end of catch	
+        
         Button button = (Button) findViewById(R.id.button1);
 		button.setOnClickListener(this);
+		
     }
 
 	public void onClick(View arg0) {
 		// TODO Auto-generated method stub
+		System.out.println("I am printing here");
+		
+		Intent selectBrandIntent = new Intent(this, SelectBrand.class);
+		startActivityForResult(selectBrandIntent, 0);
+		
+		System.out.println("printing brands names");
+		
+	 
+		
+		
+		
 		
 	}
+	
 }
