97b9d866ec4adb165f263dea2a9dd1fb26e87d66
srngirish@gmail.com <srngirish@gmail.com@6f429456-4490-fa68-301e-5b390702f549>
2012-06-27 11:04:48 +0000

    printing the brand name in logcat

--- trunk/src/com/faultcodeguide/SelectBrand.java
+++ trunk/src/com/faultcodeguide/SelectBrand.java
@@ -2,21 +2,15 @@ package com.faultcodeguide;
  
 
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import android.app.Activity;
+
 import android.app.ListActivity;
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
@@ -52,7 +46,6 @@ public class SelectBrand extends ListActivity {
 		  			  System.out.println("Integer: " + id);
 		  			  String s1= c.getString(1);
 			  			 System.out.println("Brand Name: " + s1);
-
 					  			  
                 } while (c.moveToNext());
                 
@@ -64,27 +57,46 @@ public class SelectBrand extends ListActivity {
 			setListAdapter(adapter);
 			db.close();
 			
+	   	
 			
 			
 			
-			
-//
-//try 	
-//	    	{
-//	    		
-//				db = (new DataBaseHelper(this)).getReadableDatabase();
-//				c= db.rawQuery("SELECT * FROM brand BY  `brand`.`brand_name` ASC", null);
-//				adapter = new SimpleCursorAdapter(this, R.layout.list_view, cursor, new String [] {"brand_name", "info"}, new int[] {R.id.name});
-//				setListAdapter(adapter);
-//				db.close();
-//			} catch (Exception e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//	   
+	   ListView lv = getListView();
 
-    
-   
+       // listening to single list item on click
+       lv.setOnItemClickListener(new OnItemClickListener() {
+         public void onItemClick(AdapterView<?> parent, View view,
+             int position, long id) {
+        	        	
+        	 	System.out.println("id  :");
+       	        System.out.println(id);
+       	           
+       	     String product = ((TextView) view).getText().toString();  
+       	 
+    	
+       	       
+       	        Cursor cursor = (Cursor) adapter.getItem(position);
+       	        String brand_name= cursor.getString(1);
+       	        System.out.println("Brand name"+brand_name);
+       	        
+       	        
+       	     Cursor c = (Cursor) adapter.getItem(position);   
+       	     int brand_id= c.getInt(c.getColumnIndex("_id"));
+         	System.out.printf("Brand id is %d",brand_id);
+       	        
+       	        /*
+       	     	String brand_name= cursor.getString(1);
+       	     	System.out.printf("Brand id is name"+brand_name);
+       	     */
+       	     	
+         	Intent intent = new Intent();
+        	System.out.println(" printing the series ");
+    	   	adapter.getItem(position);
+    	   	intent.putExtra("brand_id", cursor.getInt(cursor.getColumnIndex("_id")));
+    	   	System.out.println(" printing the id ");
+    	   	startActivity(intent);
+         }
+       });
 	   }  
     
 }///end of class
